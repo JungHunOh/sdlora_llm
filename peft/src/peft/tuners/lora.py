37f9b299c6255cd278131fa8878da7ae5176a906
junghunoh <junghunoh@github.com>
2025-05-15 18:28:59 +0900

    init

--- peft/src/peft/tuners/lora.py
+++ peft/src/peft/tuners/lora.py
@@ -279,7 +279,8 @@ class LoraLayer:
         self.lora_alpha = lora_alpha
         # Optional dropout
         if lora_dropout > 0.0:
-            self.lora_dropout = nn.Dropout(p=lora_dropout)
+            #self.lora_dropout = nn.Dropout(p=lora_dropout)
+            self.lora_dropout = nn.Dropout(p=0.0)
         else:
             self.lora_dropout = lambda x: x
         # Mark the weight as unmerged
@@ -375,6 +376,7 @@ class Linear(nn.Linear, LoraLayer):
             result = F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
         elif self.r > 0 and not self.merged:
             if self.r > 0:
+                result = F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
                 if self.sign_preserve:
                     matmul_output = self.lora_B.weight @ self.lora_A.weight
                     effective_w = self.weight + transpose(matmul_output.to(previous_dtype), self.fan_in_fan_out) * self.scaling
@@ -382,7 +384,6 @@ class Linear(nn.Linear, LoraLayer):
                     effective_w = abs(effective_w) * torch.sign(self.weight)
                     result = F.linear(x, transpose(effective_w, self.fan_in_fan_out), bias=self.bias)
                 else:
-                    result = F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias) + self.lora_B(self.lora_A(self.lora_dropout(x.to(self.lora_A.weight.dtype)))) * self.scaling
                     result += self.lora_B(self.lora_A(self.lora_dropout(x.to(self.lora_A.weight.dtype)))) * self.scaling
             else:
                 result = F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
