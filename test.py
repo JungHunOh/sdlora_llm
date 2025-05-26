a6f15aa21c68ee20e5f3a9dde1dec59386643f5d
junghunoh <junghunoh@github.com>
2025-05-26 16:52:55 +0900

    init

--- test.py
+++ test.py
@@ -6,17 +6,29 @@ import glob
 #lora = sorted(glob.glob('./experiment/gptj-6b-lora-math-all-r32_*.txt'))
 #slora = sorted(glob.glob('./experiment/gptj-6b-slora-math-all-r32_*.txt'))
 
-lora = sorted(glob.glob('./experiment/gpt_math_50k_lora_r32_lr0.0003_*.txt'))
-slora = sorted(glob.glob('./experiment/gpt_math_50k_slora_r32_lr0.0003_*.txt'))
+lora = sorted(glob.glob('./experiment/gpt_math_10k_lora_r16_lr0.001_*.txt'))
+slora = sorted(glob.glob('./experiment/gpt_math_10k_slora_r16_lr0.001_*.txt'))
 
 assert len(lora) == len(slora)
 
 print("LoRA vs. SLoRA")
 
-for i in range(len(lora)):
+lora_results = []
+slora_results = []
+for i in range(len(slora)):
     l = open(lora[i], 'r')
     sl = open(slora[i], 'r')
     
     print(lora[i].split('_')[-1].replace('.txt',''))
-    print(f'LoRA: {l.readlines()[-1]}')
-    print(f'SLoRA: {sl.readlines()[-1]}')
+
+    lora_result = round(float(l.readlines()[-1].split(' ')[-1]),3)
+    slora_result = round(float(sl.readlines()[-1].split(' ')[-1]),3)
+    
+    lora_results.append(lora_result)
+    slora_results.append(slora_result)
+
+    print(f'LoRA: {lora_result}')
+    print(f'SLoRA: {slora_result}')
+
+print(f'AVG LoRA: {sum(lora_results)/ len(lora_results)}')
+print(f'AVG SLoRA: {sum(slora_results)/ len(slora_results)}')
