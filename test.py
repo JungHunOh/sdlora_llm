d55511b0e6d36f28128fd21ce491ddf702c2d760
junghunoh <junghunoh@github.com>
2025-05-29 12:05:16 +0900

    init

--- test.py
+++ test.py
@@ -6,29 +6,53 @@ import glob
 #lora = sorted(glob.glob('./experiment/gptj-6b-lora-math-all-r32_*.txt'))
 #slora = sorted(glob.glob('./experiment/gptj-6b-slora-math-all-r32_*.txt'))
 
-lora = sorted(glob.glob('./experiment/gpt_math_10k_lora_r16_lr0.001_*.txt'))
-slora = sorted(glob.glob('./experiment/gpt_math_10k_slora_r16_lr0.001_*.txt'))
+#model = 'gpt'
+model = 'llama'
 
-assert len(lora) == len(slora)
+#dataset = 'math_10k'
+dataset = 'commonsense_170k'
 
-print("LoRA vs. SLoRA")
+rank = 4
+
+lr= 0.0002
+
+if dataset == 'commonsense_170k':
+    lora = sorted(glob.glob(f'./experiment/{model}_{dataset}_lora_r{rank}_lr{lr}_*result.txt'))
+    slora = sorted(glob.glob(f'./experiment/{model}_{dataset}_slora_r{rank}_lr{lr}_*result.txt'))
+else:
+    lora = sorted(glob.glob(f'./experiment/{model}_{dataset}_lora_r{rank}_lr{lr}_*.txt'))
+    slora = sorted(glob.glob(f'./experiment/{model}_{dataset}_slora_r{rank}_lr{lr}_*.txt'))
+
+print(f'\n{model}_{dataset}_r{rank}_lr{lr}')
+
+print("\n###########LoRA##########\n")
 
 lora_results = []
 slora_results = []
-for i in range(len(slora)):
+for i in range(len(lora)):
     l = open(lora[i], 'r')
-    sl = open(slora[i], 'r')
     
-    print(lora[i].split('_')[-1].replace('.txt',''))
+    print(lora[i].split('_')[-2])
 
     lora_result = round(float(l.readlines()[-1].split(' ')[-1]),3)
-    slora_result = round(float(sl.readlines()[-1].split(' ')[-1]),3)
     
     lora_results.append(lora_result)
-    slora_results.append(slora_result)
 
     print(f'LoRA: {lora_result}')
-    print(f'SLoRA: {slora_result}')
 
 print(f'AVG LoRA: {sum(lora_results)/ len(lora_results)}')
+
+print("\n##########SLoRA##########\n")
+
+for i in range(len(slora)):
+    sl = open(slora[i], 'r')
+    
+    print(slora[i].split('_')[-2])
+
+    slora_result = round(float(sl.readlines()[-1].split(' ')[-1]),3)
+    
+    slora_results.append(slora_result)
+
+    print(f'SLoRA: {slora_result}')
+
 print(f'AVG SLoRA: {sum(slora_results)/ len(slora_results)}')
